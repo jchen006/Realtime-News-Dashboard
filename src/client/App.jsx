8b5414f67322e0401b3e102f5b9d9bf8fad793a6
Jeff Chen <jeffreychen006@gmail.com>
2018-12-10 01:34:29 -0800

    converted stream to independent socket

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './app.css';
 import Stream from './TwitterFeed/Stream.jsx'
 // import GeoStreamComponent from './GeoStreamComponent.jsx'
-import io from 'socket.io-client'
+
 import Grid from '@material-ui/core/Grid'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
@@ -15,7 +15,6 @@ class App extends Component {
     super(props)
     this.state = 
     {
-      tweets: [],
       twitter: {
         filters: [
           { key: 0, label: 'Javascript' },
@@ -23,16 +22,18 @@ class App extends Component {
         lang: 'en',
         maxTweets: 5
       },
-      displayModal: false,
-      isConnecting: true
+      displayModal: false
     }
     this.handleFiltersDelete = this.handleFiltersDelete.bind(this)
     this.handleAddFilters = this.handleAddFilters.bind(this)
     // this.onUpdateMaxTweets = this.onUpdateMaxTweets.bind(this)
     // this.onUpdateFilter = this.onUpdateFilter.bind(this)
-    // this.onUpdateMaxTweets = this.onUpdateMaxTweets.bind(this)
     // this.onShowModal = this.onShowModal.bind(this)
     // this.onHideModal = this.onHideModal.bind(this)
+  }
+
+  componentDidMount() {
+
   }
 
   @keydown('shift+up')
@@ -95,28 +96,7 @@ class App extends Component {
     })
   }
 
-  componentDidMount() {
-    var endpoint = 'localhost:8080'
-    this.socket = io(endpoint)
 
-    this.socket.on('connect', () => {
-        this.setState( { isConnecting: false } )
-    })
-
-    this.socket.on('tweet', (data) => {
-        console.log(data);
-        if(this.state.tweets.length == this.state.twitter.maxTweets) {
-            var newTweetsArray = this.state.tweets.slice()
-            newTweetsArray.shift()
-            newTweetsArray.push(data)
-            this.setState( { tweets: newTweetsArray } )
-        } else if(this.state.tweets.length < this.state.twitter.maxTweets) {
-            var newTweetsArray = this.state.tweets.slice()
-            newTweetsArray.push(data)
-            this.setState( { tweets: newTweetsArray } )
-        }
-    })
-  }
 
   render() {
     const { classes } = this.props
@@ -126,7 +106,9 @@ class App extends Component {
       <div className={classes.root}>
         <Grid container spacing={24}>
           <Grid item xs={3}>
-            <Stream tweets={tweets}/>
+            <Stream 
+              maxTweets={this.state.twitter.maxTweets}
+              />
           </Grid>
         </Grid>
         <SettingsModal 
--- src/client/TwitterFeed/Stream.jsx
+++ src/client/TwitterFeed/Stream.jsx
@@ -2,14 +2,41 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import TweetCard from './TweetCard.jsx'
 import ReactLoading from 'react-loading'
+import io from 'socket.io-client'
 
 class Stream extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            isConnecting: true,
+            tweets: []
+        }
     }
 
+    componentDidMount() {
+        var endpoint = 'localhost:8080'
+        this.socket = io(endpoint)
+        this.socket.on('connect', () => {
+            this.setState( { isConnecting: false } )
+        })
+    
+        this.socket.on('tweet', (data) => {
+            console.log(this.state.tweets);
+            if(this.state.tweets.length == this.props.maxTweets) {
+                var newTweetsArray = this.state.tweets.slice()
+                newTweetsArray.shift()
+                newTweetsArray.push(data)
+                this.setState( { tweets: newTweetsArray } )
+            } else if(this.state.tweets.length < this.props.maxTweets) {
+                var newTweetsArray = this.state.tweets.concat(data)
+                this.setState( { tweets: newTweetsArray } )
+            }
+        })
+      }
+
     renderTweets() {
-        return this.props.tweets.slice().reverse().map((tweet) => {
+        console.log(this.state.tweets.length)
+        return this.state.tweets.slice().reverse().map((tweet) => {
             return (
                 <TweetCard
                     key = { tweet.user.name }
@@ -38,7 +65,7 @@ class Stream extends Component {
     render() {
         return (
             <div>
-                { this.props.tweets.length > 0 ? this.renderTweetDisplay() : this.renderLoading() }
+                { this.state.tweets.length > 0 ? this.renderTweetDisplay() : this.renderLoading() }
             </div>
         )
     }
