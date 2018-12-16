c8ae95b7ec81105f78956ef53de89128a812b3f1
Jeff Chen <jeffreychen006@gmail.com>
2018-12-16 10:54:05 -0800

    twitter live stream refactored

--- src/server/index.js
+++ src/server/index.js
@@ -1,56 +1,32 @@
 const express = require('express');
 const config = require('./config')
 const http = require('http');
-const socket = require('socket.io');
-const Twitter = require('twitter');
 const healthCheck = require('./controllers/healthCheck')
 const google = require('./controllers/google')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const twitter = require('./controllers/twitter')
 
 const app = express();
 
 app.use(express.static('../../public'));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+app.use(bodyParser.json())
 
 const port = 8080;
 
 app.get('/healthCheck', healthCheck.healthCheck)
 app.get('/google/sources', google.getAllSources)
+app.get('/google/top', google.queryTopHeadlines)
+app.get('/google/everything', google.queryEverything)
 
 const server = app.listen(port, () => {
     console.log(`Listening on port ${port}!`)
 });
 
-const io = socket(server)
-var _stream = {}
+twitter.stream(server)
 
-const client = new Twitter(config.twitter);
-var filter = {track: 'NBA'}
 
-var initiateLiveStream = function(filter) {
-    client.stream('statuses/filter', filter, function(stream) {
-        _stream = stream
-        _stream.on('data', function(event) {
-          console.log(event && event.text);
-          io.emit('tweet', event)
-        });
-       
-        _stream.on('error', function(error) {
-          throw error;
-        });
-    });
-}
-
-io.on('update', function(data) {
-    _stream.destroy()
-    if(data.filter) {
-        filter.track = data.filter
-    }
-
-    if(data.lang) {
-        filter.lang = data.lang
-    }
-    
-    initiateLiveStream(filter)
-});
 
 // initiateLiveStream(filter)
 
