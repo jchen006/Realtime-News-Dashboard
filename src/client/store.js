245f82a803496e1f84cfffa2f6c9eee3138fbb2f
Jeff Chen <jeffchen@paypal.com>
2018-12-25 08:51:57 +0800

    added logger

--- src/client/store.js
+++ src/client/store.js
@@ -1,12 +1,13 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/RootReducer';
+import logger from 'redux-logger'
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export default function configureStore(initalState={}) {
+export default function configureStore(initialState={}) {
     return createStore(
         rootReducer,
-        composeEnhancer(applyMiddleware(thunk)),
+        composeEnhancer(applyMiddleware(thunk, logger)),
     );
 }
