68dfc5a058c42e943262831c7edc1dc08a7e4e6f
Jeff Chen <jeffreychen006@gmail.com>
2019-07-10 22:52:00 -0700

    basic electron appmounting stuff

---
+++ src/dist/app.js
@@ -0,0 +1,53 @@
+const { app, BrowserWindow } = require('electron');
+const dev = process.env.NODE_ENV === 'development';
+let mainWindow;
+
+let createWindow = () => {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 800
+    });
+    mainWindow.setMenu(null);
+    mainWindow.setResizable(false);
+
+    mainWindow.loadFile('./index.html');
+
+    if (dev) {
+        // Open the DevTools.
+        mainWindow.webContents.openDevTools();
+        const {
+            default: installExtension,
+            REACT_DEVELOPER_TOOLS,
+            REDUX_DEVTOOLS,
+        } = require('electron-devtools-installer'); // eslint-disable-line
+        installExtension(REACT_DEVELOPER_TOOLS)
+            .then(name => console.log(`Added Extension:  ${name}`))
+            .catch(err => console.log('An error occurred: ', err));
+
+        installExtension(REDUX_DEVTOOLS)
+            .then(name => console.log(`Added Extension:  ${name}`))
+            .catch(err => console.log('An error occurred: ', err));
+    }
+
+    mainWindow.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
+
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
