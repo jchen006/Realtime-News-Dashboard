ad9ac84b284b85e66c6ea8e86d0aed8e23c01133
Jeff Chen <jeffreychen006@gmail.com>
2019-07-27 21:02:13 -0700

    refactored

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const http = require('http');
-const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const twitter = require('./controllers/twitter')
 const twitterRoute = require('./routes/twitter')
 const google = require('./routes/google')
 const healthCheck = require('./routes/healthCheck')
-const { morganMiddleware } = require('./middleware/morganMiddleware')
+const { morganMiddleware } = require('./middleware/logging');
 
 const app = express();
 const port = 8080;
--- src/server/middleware/logging.js
+++ src/server/middleware/logging.js
@@ -2,12 +2,10 @@ const morgan = require('morgan');
 const chalk = require('chalk');
 
 const morganMiddleware = morgan((tokens, req, res) => {
-    console.log(res.body)
     return [
         chalk.green.bold(tokens.method(req, res)),
         chalk.red.bold(tokens.status(req, res)),
         chalk.white(tokens.url(req, res)),
-        // Figure out how to log the response and request body 
         chalk.yellow(tokens['response-time'](req, res) + ' ms'),
     ].join(' ');
 })
