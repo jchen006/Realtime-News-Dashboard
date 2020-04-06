c5b9062fb27a5e744ec38a2a98fff9fa3e26df3e
Jeff Chen <jeffreychen006@gmail.com>
2020-04-05 17:18:39 -0700

    Added tooling for redux now

--- src/client/store.js
+++ src/client/store.js
@@ -2,18 +2,15 @@ import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/RootReducer';
 import logger from 'redux-logger'
+import { composeWithDevTools } from 'remote-redux-devtools'
 
-const reduxDevtools = typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION__() && window.__REDUX_DEVTOOLS_EXTENSION__();
-const composeEnhancers = reduxDevtools || compose;
+const composeEnhancers = composeWithDevTools({ realtime: true, port: 8000 })
 
 export const configureStore = (initialState={}) => {
     const middlewares = [thunk, logger];
     return createStore(
         rootReducer,
         initialState,
-        // composeEnhancers(applyMiddleware(...middlewares)),
+        composeEnhancers(applyMiddleware(...middlewares)),
     );
 }
-
-
-
